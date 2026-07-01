464c19e9af6c8bf4fc8cccacd79f277c25520e06
Yvens Rebouças Serpa <y.reboucasserpa@saxion.nl>
2026-07-01 17:37:06 +0200

    Filter action maps based on selected input action maps

--- WavesGame/Assets/Scripts/DCF/GenericInputTracer.cs
+++ WavesGame/Assets/Scripts/DCF/GenericInputTracer.cs
@@ -1,12 +1,17 @@
 /*
  * Copyright (c) 2026 Yvens R Serpa [https://github.com/YvensFaos/]
- * 
+ *
  * This work is licensed under the Creative Commons Attribution 4.0 International License.
  * To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/
  * or see the LICENSE file in the root directory of this repository.
  */
+
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NaughtyAttributes;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Utilities;
 using UUtils;
 
@@ -14,16 +19,43 @@ namespace DCF
 {
     public abstract class GenericInputTracer : WeakSingleton<GenericInputTracer>
     {
-        [SerializeField]
+        [SerializeField] private InputActionAsset inputActionAsset;
+        [SerializeField] private List<string> trackingMaps;
         private InputActionTrace _tracer;
-
         private bool _tracing;
+
         private void Start()
         {
             _tracer = new InputActionTrace();
-            _tracer.SubscribeToAll();
+
+            if (inputActionAsset == null)
+            {
+                _tracer.SubscribeToAll();
+            }
+            else
+            {
+                if (trackingMaps.Count == 0)
+                {
+                    foreach (var inputActionMap in inputActionAsset.actionMaps)
+                    {
+                        _tracer.SubscribeTo(inputActionMap);
+                    }
+                }
+                else
+                {
+                    trackingMaps.ForEach(trackingMap =>
+                    {
+                        foreach (var inputActionMap in inputActionAsset.actionMaps.Where(
+                                     t => t.name.Equals(trackingMap)))
+                        {
+                            _tracer.SubscribeTo(inputActionMap);
+                        }
+                    });
+                }
+            }
+
             _tracing = true;
-            
+
             StartCoroutine(Tracing());
         }
 
@@ -39,12 +71,24 @@ namespace DCF
                     var input = inputEnumerator.Current;
                     ProcessActionEventPtr(input);
                 }
+
                 inputEnumerator.Dispose();
                 _tracer.Clear();
-            } 
+            }
+
             yield return null;
         }
 
         protected abstract void ProcessActionEventPtr(InputActionTrace.ActionEventPtr actionEventPtr);
+
+        [Button("Load Maps")]
+        public void LoadMaps()
+        {
+            trackingMaps.Clear();
+            foreach (var inputActionMap in inputActionAsset.actionMaps)
+            {
+                trackingMaps.Add(inputActionMap.name);
+            }
+        }
     }
 }
