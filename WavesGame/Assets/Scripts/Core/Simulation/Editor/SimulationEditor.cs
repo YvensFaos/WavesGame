9b9a654003151ec13691a5351e9ad561e379990f
Yvens Rebouças Serpa <y.reboucasserpa@saxion.nl>
2026-09-15 14:41:02 +0200

    Add extra LLMs to test and improve simulation editor viewer

--- WavesGame/Assets/Scripts/Core/Simulation/Editor/SimulationEditor.cs
+++ WavesGame/Assets/Scripts/Core/Simulation/Editor/SimulationEditor.cs
@@ -15,15 +15,35 @@ namespace Core.Simulation.Editor
     [CustomEditor(typeof(Simulation))]
     public class SimulationEditor : UnityEditor.Editor
     {
+        private UnityEditor.Editor _scriptableObjectEditor;
+        
         public override void OnInspectorGUI()
         {
             DrawDefaultInspector();
 
             EditorGUILayout.Space(10);
+            if (GUILayout.Button("Ping in Project"))
+            {
+                EditorGUIUtility.PingObject(target);
+                Selection.activeObject = target;
+            }
             var simulation = (Simulation)target;
             if (GUILayout.Button("Rename Simulation"))
             {
                 RenameScriptableObjectHelper.RenameAssetFile(simulation, simulation.ToString());
+            }
+            
+            EditorGUILayout.Space(15);
+            var simulationFactionPlayerTypePairs = simulation.FactionPlayerTypePairs;
+            foreach (var pair in simulationFactionPlayerTypePairs)
+            {
+                // EditorGUILayout.HelpBox($"{pair.One.name}", MessageType.Info);
+                var rect = EditorGUILayout.BeginVertical();
+                var faction = pair.One;
+                EditorGUI.DrawRect(rect, faction.factionColor); // green tint
+                CreateCachedEditor(pair.Two, null, ref _scriptableObjectEditor);
+                _scriptableObjectEditor.OnInspectorGUI();
+                EditorGUILayout.EndVertical();
             }
         }
     }
